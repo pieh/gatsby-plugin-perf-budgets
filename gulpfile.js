0bdc82baf67243fb071a13190d767e02d28f7206
Michal Piechowiak <misiek.piechowiak@gmail.com>
2021-04-29 23:05:41 +0200

    make built scripts actually work

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,38 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
+const gulp = require("gulp");
 
-const NODE_SRC = './src/**/*.js';
-const NODE_DEST = './lib';
+const NODE_SRC = "./src/**/*.js";
+const NODE_DEST = "./lib";
 
-const cli = require('commander')
-  .usage('<task> [options]')
-  .option('-e, --env <environment>', 'Can be `prod` or `dev`. Default is `dev`', /^(dev|prod)$/u, 'dev')
-  .option('-a, --analyze', 'Analyze client bundle. If set, `env` will be set to `prod`.')
+const cli = require("commander")
+  .usage("<task> [options]")
+  .option(
+    "-e, --env <environment>",
+    "Can be `prod` or `dev`. Default is `dev`",
+    /^(dev|prod)$/u,
+    "dev"
+  )
+  .option(
+    "-a, --analyze",
+    "Analyze client bundle. If set, `env` will be set to `prod`."
+  )
   .parse(process.argv);
 
-const task = cli.args[0] || 'watch';
-if (task === 'build' || cli.analyze) {
-  cli.env = 'prod';
-}
+const task = cli.args[0] || "watch";
 
-gulp.task('clean', gulp.parallel(cleanNodeScripts, cleanViewerScripts));
-gulp.task('build', gulp.series('clean', compileNodeScripts, compileViewerScripts));
-gulp.task('watch', gulp.series('build', watch));
-gulp.task('default', gulp.task('watch'));
+gulp.task("clean", gulp.parallel(cleanNodeScripts, cleanViewerScripts));
+gulp.task(
+  "build",
+  gulp.series("clean", compileNodeScripts, compileViewerScripts)
+);
+gulp.task("watch", gulp.series("build", watch));
+gulp.task("default", gulp.task("watch"));
 
 class TaskError extends Error {
   constructor(message) {
     super(message);
-    this.name = 'TaskError';
+    this.name = "TaskError";
     // Internal Gulp flag that says "don't display error stack trace"
     this.showStack = false;
   }
@@ -34,54 +42,50 @@ function watch() {
   gulp
     .watch(NODE_SRC, gulp.series(cleanNodeScripts, compileNodeScripts))
     // TODO: replace with `emitErrors: false` option after https://github.com/gulpjs/glob-watcher/pull/34 will be merged
-    .on('error', () => {});
+    .on("error", () => {});
 }
 
 function cleanViewerScripts() {
-  const del = require('del');
-  return del('public');
+  const del = require("del");
+  return del("public");
 }
 
 function cleanNodeScripts() {
-  const del = require('del');
+  const del = require("del");
   return del(NODE_DEST);
 }
 
 function compileNodeScripts() {
-  const babel = require('gulp-babel');
+  const babel = require("gulp-babel");
 
-  return gulp
-    .src(NODE_SRC)
-    .pipe(babel())
-    .pipe(gulp.dest(NODE_DEST));
+  return gulp.src(NODE_SRC).pipe(babel()).pipe(gulp.dest(NODE_DEST));
 }
 
 function compileViewerScripts() {
-  const webpack = require('webpack');
-  const config = require('./webpack.config')({
+  const webpack = require("webpack");
+  const config = require("./webpack.config")({
     env: cli.env,
-    analyze: cli.analyze
+    analyze: cli.analyze,
+    watch: task === `dev`,
   });
 
   return new Promise((resolve, reject) => {
     webpack(config, (err, stats) => {
-      if (cli.env === 'dev') {
+      if (cli.env === "dev") {
         if (err) {
           console.error(err);
         } else {
-          console.log(stats.toString({colors: true}));
+          console.log(stats.toString({ colors: true }));
         }
         resolve();
       } else {
         if (err) return reject(err);
 
         if (stats.hasErrors()) {
-          reject(
-            new TaskError('Webpack compilation error')
-          );
+          reject(new TaskError("Webpack compilation error"));
         }
 
-        console.log(stats.toString({colors: true}));
+        console.log(stats.toString({ colors: true }));
         resolve();
       }
     });
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
-const compact = require('lodash/compact');
-const webpack = require('webpack');
-const TerserPlugin = require('terser-webpack-plugin');
-const BundleAnalyzePlugin = require('./lib/BundleAnalyzerPlugin');
+const compact = require("lodash/compact");
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const BundleAnalyzePlugin = require("./lib/BundleAnalyzerPlugin");
 
-module.exports = opts => {
-  opts = Object.assign({
-    env: 'dev',
-    analyze: false
-  }, opts);
+module.exports = (opts) => {
+  opts = Object.assign(
+    {
+      env: "dev",
+      analyze: false,
+      watch: false,
+    },
+    opts
+  );
 
-  const isDev = (opts.env === 'dev');
+  const isDev = opts.env === "dev";
 
   return {
-    mode: isDev ? 'development' : 'production',
+    mode: isDev ? "development" : "production",
     context: __dirname,
-    entry: './client/viewer',
+    entry: "./client/viewer",
     output: {
       path: `${__dirname}/public`,
-      filename: 'viewer.js',
-      publicPath: '/'
+      filename: "viewer.js",
+      publicPath: "/",
     },
 
     resolve: {
-      modules: [
-        `${__dirname}/client/vendor`,
-        'node_modules'
-      ],
-      extensions: ['.js', '.jsx'],
+      modules: [`${__dirname}/client/vendor`, "node_modules"],
+      extensions: [".js", ".jsx"],
       alias: {
-        mobx: require.resolve('mobx/lib/mobx.es6.js')
-      }
+        mobx: require.resolve("mobx/lib/mobx.es6.js"),
+      },
     },
 
-    devtool: isDev ? 'eval' : 'source-map',
-    watch: isDev,
+    devtool: isDev ? "eval" : "source-map",
+    watch: opts.watch,
 
     optimization: {
       minimize: !isDev,
@@ -43,12 +44,12 @@ module.exports = opts => {
           sourceMap: true,
           terserOptions: {
             output: {
-              comments: /copyright/iu
+              comments: /copyright/iu,
             },
-            safari10: true
-          }
-        })
-      ]
+            safari10: true,
+          },
+        }),
+      ],
     },
 
     module: {
@@ -56,94 +57,100 @@ module.exports = opts => {
         {
           test: /\.jsx?$/u,
           exclude: /(node_modules|client\/vendor)/u,
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             babelrc: false,
             presets: [
-              ['@babel/preset-env', {
-                // Target browsers are specified in .browserslistrc
+              [
+                "@babel/preset-env",
+                {
+                  // Target browsers are specified in .browserslistrc
 
-                modules: false,
-                useBuiltIns: 'usage',
-                corejs: 2,
-                exclude: [
-                  // Excluding unused polyfills to completely get rid of `core.js` in the resulting bundle
-                  'web.dom.iterable',
-                  'es7.symbol.async-iterator'
-                ],
-                debug: true
-              }],
-              '@babel/preset-react'
+                  modules: false,
+                  useBuiltIns: "usage",
+                  corejs: 2,
+                  exclude: [
+                    // Excluding unused polyfills to completely get rid of `core.js` in the resulting bundle
+                    "web.dom.iterable",
+                    "es7.symbol.async-iterator",
+                  ],
+                  debug: true,
+                },
+              ],
+              "@babel/preset-react",
             ],
             plugins: [
-              'lodash',
-              ['@babel/plugin-proposal-decorators', {legacy: true}],
-              ['@babel/plugin-proposal-class-properties', {loose: true}],
-              ['@babel/plugin-transform-runtime', {
-                useESModules: true
-              }]
-            ]
-          }
+              "lodash",
+              ["@babel/plugin-proposal-decorators", { legacy: true }],
+              ["@babel/plugin-proposal-class-properties", { loose: true }],
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  useESModules: true,
+                },
+              ],
+            ],
+          },
         },
         {
           test: /\.css$/u,
           use: [
-            'style-loader',
+            "style-loader",
             {
-              loader: 'css-loader',
+              loader: "css-loader",
               options: {
                 modules: true,
-                localIdentName: '[name]__[local]',
-                importLoaders: 1
-              }
+                localIdentName: "[name]__[local]",
+                importLoaders: 1,
+              },
             },
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
               options: {
                 plugins: compact([
-                  require('postcss-icss-values'),
-                  require('autoprefixer'),
-                  !isDev && require('cssnano')()
-                ])
-              }
-            }
-          ]
+                  require("postcss-icss-values"),
+                  require("autoprefixer"),
+                  !isDev && require("cssnano")(),
+                ]),
+              },
+            },
+          ],
         },
         {
           test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/u,
-          loader: 'url-loader'
+          loader: "url-loader",
         },
         {
           test: /carrotsearch\.foamtree/u,
-          loader: 'exports-loader?CarrotSearchFoamTree'
-        }
-      ]
+          loader: "exports-loader?CarrotSearchFoamTree",
+        },
+      ],
     },
 
-    plugins: (plugins => {
+    plugins: ((plugins) => {
       if (!isDev) {
         if (opts.analyze) {
           plugins.push(
             new BundleAnalyzePlugin({
-              generateStatsFile: true
+              generateStatsFile: true,
             })
           );
         }
 
         plugins.push(
           new webpack.DefinePlugin({
-            'process': JSON.stringify({
+            process: JSON.stringify({
               env: {
-                NODE_ENV: 'production'
-              }
+                NODE_ENV: "production",
+              },
             }),
             // Fixes "ModuleConcatenation bailout" for some modules (e.g. Preact and MobX)
-            'global': 'undefined'
+            global: "undefined",
           })
         );
       }
 
       return plugins;
-    })([])
+    })([]),
   };
 };
